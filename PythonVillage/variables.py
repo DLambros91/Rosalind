90370f2101c8771db01a13b0259645410110bf38
DLambros91 <d.lambropoulos91@gmail.com>
2015-01-12 16:03:16 -0500

    Update variables.py

--- PythonVillage/variables.py
+++ PythonVillage/variables.py
@@ -1,14 +1,14 @@
-#########################################################
-# Author: Dlambros #
-# Date: Sun, Dec 28, 2014 #
-# #
+##############################################################
+# Author: Dlambros                                           #
+# Date: Sun, Dec 28, 2014                                    #
+#                                                            #
 # Given: Two positive integers a and b, each less than 1000. #
-# #
-# Return: The integer corresponding to the square of the  #
-#         hypotenuse of the right triangle whose legs have #
-#         lengths a and b.    #
-# #
-#########################################################
+#                                                            #
+# Return: The integer corresponding to the square of the     #
+#         hypotenuse of the right triangle whose legs have   #
+#         lengths a and b.                                   #
+# 							     #
+##############################################################
 
 sides = []
 with open('rosalind_ini2.txt', 'r') as f:
